b821a0ce0f01438e255925835ae7e18159b53074
mart-anthony-stark <martsalazar0@gmail.com>
2021-12-25 20:34:27 +0800

    Add create list method

--- src/app/pages/task-view/task-view.component.ts
+++ src/app/pages/task-view/task-view.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { TaskService } from 'src/app/task.service';
 
 @Component({
   selector: 'app-task-view',
   templateUrl: './task-view.component.html',
-  styleUrls: ['./task-view.component.scss']
+  styleUrls: ['./task-view.component.scss'],
 })
 export class TaskViewComponent implements OnInit {
+  constructor(private taskService: TaskService) {}
 
-  constructor() { }
+  public lists: [{ title: String; _id: String }] = [{ title: '', _id: '' }];
 
   ngOnInit(): void {
+    this.getLists();
   }
 
+  createNewList() {
+    this.taskService.createList('List').subscribe((response: any) => {
+      this.getLists();
+    });
+  }
+
+  getLists() {
+    this.taskService.getLists().subscribe((response: any) => {
+      this.lists = response;
+    });
+  }
 }
