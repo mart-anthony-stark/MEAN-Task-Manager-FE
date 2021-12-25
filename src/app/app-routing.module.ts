e39ba08ed2087cc4e83de6ee9da8ab6b993bfe85
mart-anthony-stark <martsalazar0@gmail.com>
2021-12-25 21:47:31 +0800

    Add paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NewListComponent } from './pages/new-list/new-list.component';
 import { TaskViewComponent } from './pages/task-view/task-view.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/tasks', pathMatch: 'full' },
-  { path: 'tasks', component: TaskViewComponent },
+  { path: '', redirectTo: '/lists', pathMatch: 'full' },
+  { path: 'lists', component: TaskViewComponent },
+  { path: 'lists/:id', component: TaskViewComponent },
+  { path: 'new-list', component: NewListComponent },
 ];
 
 @NgModule({
