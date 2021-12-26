f4413ff5364b7131df4ba6d0ab13d658e8f15a9f
mart-anthony-stark <martsalazar0@gmail.com>
2021-12-26 14:27:22 +0800

    Add new task route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NewListComponent } from './pages/new-list/new-list.component';
+import { NewTaskComponent } from './pages/new-task/new-task.component';
 import { TaskViewComponent } from './pages/task-view/task-view.component';
 
 const routes: Routes = [
@@ -8,6 +9,7 @@ const routes: Routes = [
   { path: 'lists', component: TaskViewComponent },
   { path: 'lists/:id', component: TaskViewComponent },
   { path: 'new-list', component: NewListComponent },
+  { path: 'lists/:id/new-task', component: NewTaskComponent },
 ];
 
 @NgModule({
