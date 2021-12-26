3dec2d49444cbad46b90d8cb6c67459e44cbab1c
mart-anthony-stark <martsalazar0@gmail.com>
2021-12-26 21:12:52 +0800

    Add login component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,10 @@ import { TaskViewComponent } from './pages/task-view/task-view.component';
 import { HttpClientModule } from '@angular/common/http';
 import { NewListComponent } from './pages/new-list/new-list.component';
 import { NewTaskComponent } from './pages/new-task/new-task.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
 
 @NgModule({
-  declarations: [AppComponent, TaskViewComponent, NewListComponent, NewTaskComponent],
+  declarations: [AppComponent, TaskViewComponent, NewListComponent, NewTaskComponent, LoginPageComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
