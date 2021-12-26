45679b9d1af1ad6effa4515c94fc01bec386b2a6
mart-anthony-stark <martsalazar0@gmail.com>
2021-12-26 19:57:11 +0800

    Add task model

--- src/app/models/task.model.ts
+++ src/app/models/task.model.ts
@@ -1,5 +1,6 @@
-export interface Task {
-  _id: string;
-  title: string;
-  _listId: string;
+export class Task {
+  _id!: string;
+  title!: string;
+  _listId!: string;
+  completed?: boolean;
 }
