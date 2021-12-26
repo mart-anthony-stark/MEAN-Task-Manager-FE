42355c1767ce3da1ba3223a2933a44e999faa9d8
mart-anthony-stark <martsalazar0@gmail.com>
2021-12-26 14:27:32 +0800

    Add new task component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,10 @@ import { TaskViewComponent } from './pages/task-view/task-view.component';
 
 import { HttpClientModule } from '@angular/common/http';
 import { NewListComponent } from './pages/new-list/new-list.component';
+import { NewTaskComponent } from './pages/new-task/new-task.component';
 
 @NgModule({
-  declarations: [AppComponent, TaskViewComponent, NewListComponent],
+  declarations: [AppComponent, TaskViewComponent, NewListComponent, NewTaskComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
