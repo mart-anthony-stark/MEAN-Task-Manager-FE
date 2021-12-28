fd08a4340f2ad3e06729990bfebabda87c63ac64
mart-anthony-stark <martsalazar0@gmail.com>
2021-12-28 22:07:20 +0800

    Add login

--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -1,15 +1,22 @@
+import { HttpResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/auth.service';
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
-  styleUrls: ['./login-page.component.scss']
+  styleUrls: ['./login-page.component.scss'],
 })
 export class LoginPageComponent implements OnInit {
+  constructor(private authService: AuthService) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onLoginClick(email: string, password: string) {
+    this.authService
+      .login(email, password)
+      .subscribe((res: HttpResponse<any>) => {
+        console.log(res);
+      });
   }
-
 }
