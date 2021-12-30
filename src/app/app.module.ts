f7c4bd00c627dffcca79967b98baf9bd242ab940
mart-anthony-stark <martsalazar0@gmail.com>
2021-12-30 20:35:03 +0800

    Add web request interceptor to providers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TaskViewComponent } from './pages/task-view/task-view.component';
 
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NewListComponent } from './pages/new-list/new-list.component';
 import { NewTaskComponent } from './pages/new-task/new-task.component';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { WebReqInterceptor } from './web-req.interceptor.service';
 
 @NgModule({
-  declarations: [AppComponent, TaskViewComponent, NewListComponent, NewTaskComponent, LoginPageComponent],
+  declarations: [
+    AppComponent,
+    TaskViewComponent,
+    NewListComponent,
+    NewTaskComponent,
+    LoginPageComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: WebReqInterceptor, multi: true },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
