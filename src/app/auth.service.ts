acaf1afd2e692fd067f23e09ce0b30afc12801d8
mart-anthony-stark <martsalazar0@gmail.com>
2021-12-30 20:35:24 +0800

    Add setAccessToken

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,60 +1,100 @@
-import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Router } from '@angular/router';
-import { shareReplay, tap } from 'rxjs';
+import { HttpClient, HttpResponse } from '@angular/common/http';
 import { WebRequestService } from './web-request.service';
+import { Router } from '@angular/router';
+import { shareReplay, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  constructor(private webService: WebRequestService, private router: Router) {}
+  constructor(
+    private webService: WebRequestService,
+    private router: Router,
+    private http: HttpClient
+  ) {}
 
   login(email: string, password: string) {
     return this.webService.login(email, password).pipe(
       shareReplay(),
       tap((res: HttpResponse<any>) => {
-        // auth tokens will be in the header of response
+        // the auth tokens will be in the header of this response
         this.setSession(
           res.body._id,
-          res.headers.get('x-access-token'),
-          res.headers.get('x-refresh-token')
+          res.headers.get('x-access-token') || '',
+          res.headers.get('x-refresh-token') || ''
         );
+        console.log('LOGGED IN!');
       })
     );
   }
 
-  getAccessToken(){
-    return localStorage.getItem('x-access-token')
-  }
-  
-  setAccessToken(accessToken: string){
-    return localStorage.setItem('x-access-token', accessToken)
+  // signup(email: string, password: string) {
+  //   return this.webService.signup(email, password).pipe(
+  //     shareReplay(),
+  //     tap((res: HttpResponse<any>) => {
+  //       // the auth tokens will be in the header of this response
+  //       this.setSession(
+  //         res.body._id,
+  //         res.headers.get('x-access-token') || '',
+  //         res.headers.get('x-refresh-token') || ''
+  //       );
+  //       console.log('Successfully signed up and now logged in!');
+  //     })
+  //   );
+  // }
+
+  logout() {
+    this.removeSession();
+
+    this.router.navigate(['/login']);
   }
 
-  getRefreshToken(){
-    return localStorage.getItem('x-refresh-token')
+  getAccessToken() {
+    return localStorage.getItem('x-access-token') || '';
   }
 
-  setRefreshToken(refreshToken: string){
-    return localStorage.setItem('x-refresh-token', refreshToken)
+  getRefreshToken() {
+    return localStorage.getItem('x-refresh-token') || '';
   }
 
-  private setSession(userId: string, accessToken: any, refreshToken: any) {
+  getUserId() {
+    return localStorage.getItem('user-id') || '';
+  }
+
+  setAccessToken(accessToken: string) {
+    localStorage.setItem('x-access-token', accessToken);
+  }
+
+  private setSession(
+    userId: string,
+    accessToken: string,
+    refreshToken: string
+  ) {
     localStorage.setItem('user-id', userId);
-    localStorage.setItem('access-token', accessToken);
-    localStorage.setItem('refresh-token', refreshToken);
+    localStorage.setItem('x-access-token', accessToken);
+    localStorage.setItem('x-refresh-token', refreshToken);
   }
 
   private removeSession() {
     localStorage.removeItem('user-id');
-    localStorage.removeItem('access-token');
-    localStorage.removeItem('refresh-token');
+    localStorage.removeItem('x-access-token');
+    localStorage.removeItem('x-refresh-token');
   }
 
-  logout() {
-    this.removeSession();
+  getNewAccessToken() {
+    return this.http
+      .get(`${this.webService.ROOT_URL}/users/me/access-token`, {
+        headers: {
+          'x-refresh-token': this.getRefreshToken(),
+          _id: this.getUserId(),
+        },
+        observe: 'response',
+      })
+      .pipe(
+        tap((res: HttpResponse<any>) => {
+          this.setAccessToken(res.headers.get('x-access-token') || 'access');
+        })
+      );
   }
-
-
 }
