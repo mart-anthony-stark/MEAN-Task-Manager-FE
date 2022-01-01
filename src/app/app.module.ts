5f2046facbeaa229ba1c09e48b3a4e863e149fc2
mart-anthony-stark <martsalazar0@gmail.com>
2022-01-01 15:54:10 +0800

    Add signup page component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { NewListComponent } from './pages/new-list/new-list.component';
 import { NewTaskComponent } from './pages/new-task/new-task.component';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { WebReqInterceptor } from './web-req.interceptor.service';
+import { SignupPageComponent } from './pages/signup-page/signup-page.component';
 
 @NgModule({
   declarations: [
@@ -18,6 +19,7 @@ import { WebReqInterceptor } from './web-req.interceptor.service';
     NewListComponent,
     NewTaskComponent,
     LoginPageComponent,
+    SignupPageComponent,
   ],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [
